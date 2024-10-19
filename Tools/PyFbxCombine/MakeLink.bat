388980e4696bdd3cebc912543f953af93f28acce
billwillman <billwillman@qq.com>
2024-10-20 01:34:40 +0800

    111

---
+++ Tools/PyFbxCombine/MakeLink.bat
@@ -0,0 +1,3 @@
+@echo off
+echo CurrentDir: %~dp0
+mklink /d "%~dp0/PyFbxCombine/.venv/Scripts" "C:\Users\zengyi\AppData\Local\Programs\Python\Python313"
--- Tools/PyFbxCombine/PyFbxCombine/PyFBXCombine.py
+++ Tools/PyFbxCombine/PyFbxCombine/PyFBXCombine.py
@@ -1,1 +1,22 @@
-import numpy
+import numpy as np
+from objloader import Obj
+
+def BuildFBXData(objFileName):
+    model = Obj.open(objFileName)
+    ## 位置数据
+    vertexs = np.array(model.vertices)
+    ## 法线
+    normals = np.array(model.normals)
+    ## 纹理坐标
+    texcoords = np.array(model.faces)
+    ## 三角面索引列表
+    faces = np.array(model.faces)
+    return
+
+def Main():
+    return
+
+##################################### 调用入口 ###################################
+if __name__ == '__main__':
+    Main()
+#################################################################################
