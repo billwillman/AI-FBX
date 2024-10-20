16b4c242c457b24fe2bdf899874637e165d53922
billwillman <billwillman@qq.com>
2024-10-20 22:24:59 +0800

    1111

--- Tools/PyFbxCombine/PyFbxCombine/PyFBXCombine.py
+++ Tools/PyFbxCombine/PyFbxCombine/PyFBXCombine.py
@@ -27,7 +27,7 @@ def CreateMesh(scene, meshName, vertexs, normals, texcoords, faces):
     rootNode = scene.GetRootNode()
     currentNode = FbxNode.Create(scene, meshName)
 
-    mesh = FbxMesh.Create(scene, meshName)
+    mesh: FbxMesh = FbxMesh.Create(scene, meshName)
 
     ## 顶点
     vertexNum = len(vertexs)
@@ -35,21 +35,10 @@ def CreateMesh(scene, meshName, vertexs, normals, texcoords, faces):
     for idx in range(0, vertexNum, 1):
         vert = FbxVector4(vertexs[idx][0], vertexs[idx][1], vertexs[idx][2])
         mesh.SetControlPointAt(vert, idx)
-    ## 法线
-    normalNum = len(normals)
-    if normalNum > 0:
-        '''
-        normalElements: FbxLayerElementNormal = mesh.CreateElementNormal()
-        normalElements.SetMappingMode(FbxLayerElementNormal.EMappingMode.eByControlPoint)
-        normalElements.SetReferenceMode(FbxLayerElementNormal.EReferenceMode.eDirect)
-        '''
-        mesh.InitNormals(normalNum)
-        for idx in range(0, normalNum, 1):
-            normal = FbxVector4(normals[idx][0], normals[idx][1], normals[idx][2])
-            mesh.SetControlPointNormalAt(normal, idx)
     ## subMesh(IndexBuffer), 有可能有多个subMesh的
     faceNum = len(faces)
     idx = 0
+    faceSubIdx = 0
     while idx < faceNum:
         face1 = faces[idx]
         idx += 1
@@ -60,12 +49,40 @@ def CreateMesh(scene, meshName, vertexs, normals, texcoords, faces):
         # 顶点索引
         mesh.BeginPolygon()
         ## 从1开始的，转成从0开始
-        mesh.AddPolygon(face1[0] - 1)
-        mesh.AddPolygon(face2[0] - 1)
-        mesh.AddPolygon(face3[0] - 1)
+        mesh.AddPolygon(face1[faceSubIdx] - 1)
+        mesh.AddPolygon(face2[faceSubIdx] - 1)
+        mesh.AddPolygon(face3[faceSubIdx] - 1)
         mesh.EndPolygon()
-    ##
+    ## 法线
+    faceSubIdx += 1
+    if len(normals) > 0:
+        faceSubIdx += 1
+    ## UV（也有可能有多个，这个暂时只支持1个）
+    if False:
+        uvNum = len(texcoords)
+        if uvNum > 0:
+            uv_layer = mesh.CreateElementUV("_MainTex")
+            uv_array = uv_layer.GetDirectArray()
+            uv_array.Resize(uvNum)
+            for i in range(0, uvNum, 1):
+                uv_array.SetAt(i, FbxVector2(texcoords[i][0], texcoords[i][1]))
+            uv_index_array = uv_layer.GetIndexArray()
+            uv_index_array.Resize(faceNum)
+            idx = 0
+            while idx < faceNum:
+                face1 = faces[idx]
+                uv_index_array.SetAt(idx, face1[faceSubIdx] - 1)
+                idx += 1
+                face2 = faces[idx]
+                uv_index_array.SetAt(idx, face2[faceSubIdx] - 1)
+                idx += 1
+                face3 = faces[idx]
+                uv_index_array.SetAt(idx, face3[faceSubIdx] - 1)
+                idx += 1
 
+            mesh.SetLayer(0, uv_layer)
+
+    mesh.BuildMeshEdgeArray() # 生成边界数组
     currentNode.AddNodeAttribute(mesh)
 
     rootNode.AddChild(currentNode)
