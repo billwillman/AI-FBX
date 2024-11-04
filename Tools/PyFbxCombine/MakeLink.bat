b9896b59c0dd9541234d27a13e132f357662cb44
bestzeng <bestzeng@tencent.com>
2024-11-04 10:58:36 +0800

    1111

--- Tools/PyFbxCombine/MakeLink.bat
+++ Tools/PyFbxCombine/MakeLink.bat
@@ -1,3 +1,3 @@
 @echo off
 echo CurrentDir: %~dp0
-mklink /d "%~dp0PyFbxCombine/venv/Scripts" "C:\Users\bestzeng\AppData\Local\Programs\Python\Python310"
+mklink /d "%~dp0PyFbxCombine/.venv/Scripts" "C:\Users\bestzeng\AppData\Local\Programs\Python\Python310"
