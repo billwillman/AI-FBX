979edfa9a8eac68847fd45eb3b7a233fe3ab8c21
billwillman <billwillman@qq.com>
2024-11-05 02:14:02 +0800

    1111

--- Tools/PyFbxCombine/PyFbxCombine/PyFBXCombine.py
+++ Tools/PyFbxCombine/PyFbxCombine/PyFBXCombine.py
@@ -6,6 +6,7 @@ import os
 import numpy as np
 from objloader import Obj
 from fbx import *
+from scipy.spatial.transform import Rotation as R
 import FbxCommon
 from operator import index
 from test.test_importlib.import_.test_fromlist import ReturnValue
@@ -129,31 +130,9 @@ def _CreateQuatFronAxisDegree(axis: FbxDouble3, degree: float)->FbxQuaternion:
     return ret
 
 def _QuatToRollPitchYaw(quat: FbxQuaternion)->FbxDouble3:
-    x = quat[0]
-    y = quat[1]
-    z = quat[2]
-    w = quat[3]
-    t0 = 2.0 * (w * x + y * z)
-    t1 = 1.0 - 2.0 * (x * x + y * y)
-    roll_x = math.atan2(t0, t1) * 180.0/math.pi
-    if math.fabs(roll_x) <= 0.000001:
-        roll_x = 0
-
-    t2 = 2.0 * (w * y - z * x)
-    t2 = 1.0 if t2 > 1.0 else t2
-    t2 = -1.0 if t2 < -1.0 else t2
-    pitch_y = math.asin(t2) * 180.0/math.pi
-    if math.fabs(pitch_y) <= 0.000001:
-        pitch_y = 0
-
-    t3 = 2.0 * (w * z + x * y)
-    t4 = 1.0 - 2.0 * (y * y + z * z)
-    yaw_z = math.atan2(t3, t4) * 180.0/math.pi
-    if math.fabs(yaw_z) <= 0.000001:
-        yaw_z = 0
-
-    ret = FbxDouble3(roll_x, pitch_y, yaw_z)
-    return ret
+    rr = R.from_quat([quat[0], quat[1], quat[2], quat[3]])
+    ret = rr.as_euler("zxy", True)
+    return FbxDouble3(ret[1], ret[2], ret[0])
 
 def _RollPitchYawToQuat(degrees: FbxDouble3)->FbxQuaternion:
     cr = math.cos(degrees[0] * math.pi / 180.0 * 0.5)
@@ -613,7 +592,6 @@ def Main():
     subRot = _RelativeDegree(parenteRot, rot)
     print(subRot[0], subRot[1], subRot[2])
     return
-    '''
     ##print(type(None))
     '''
         BuildFBXData(objFileName, vertBoneDataFileName, boneLocDataFileName, boneRotDataFileName, boneScaleDataFileName,
